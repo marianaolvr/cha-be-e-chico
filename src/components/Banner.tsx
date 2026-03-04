2cb312e6722c30eab26ae97f563ed484c603d741
Mariana Oliveira Alves <marianaoliveiraalves@Marianas-MacBook-Air.local>
2026-03-04 15:03:19 -0300

    chore: fix image

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ import bannerImg from "@/assets/banner-cha-bee-chico.png";
 
 const Banner = () => {
   return (
-    <section className="relative w-full bg-background overflow-hidden">
+    <section className="relative w-full bg-background overflow-hidden pt-12">
       <div className="max-w-3xl mx-auto md:max-w-2xl md:px-8">
         <div className="overflow-hidden rounded-lg">
           <img
